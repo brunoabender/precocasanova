377473833ddce0d1ff4cf7aa7273d83cfadc3187
Bruno Bender <brunoabender@live.com>
2025-05-05 01:42:48 -0300

    primeira versao

--- ScrapperPreços/ScrapperPreços/obj/Debug/net8.0/ScrapperPreços.AssemblyInfo.cs
+++ ScrapperPreços/ScrapperPreços/obj/Debug/net8.0/ScrapperPreços.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrapperPreços")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0758503f003b114efdb1919aedf87911353e3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrapperPreços")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrapperPreços")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
